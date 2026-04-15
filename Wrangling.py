5905b4a344477673c5df81eb8a58dda71b1cc6ee
Yves Noé Bornhauser <Yves.bornhauser@students.fhnw.ch>
2026-04-15 13:46:50 +0200

    Updated the script for the new data coming from the PDF and binary for Piste 10

--- Wrangling.py
+++ Wrangling.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import holidays 
+import numpy as np
 
 
 #Verspätungsdaten als Dataframe setzen und nach Zürich filtern
@@ -13,8 +14,10 @@ df_abfluege = pd.read_csv("zrh_abfluege_pro_tag.csv")
 
 #Datum korrekt formatieren für den Merge, da das Jahr in der CSV nur zweistellig ist
 #Dies weil es sonst extrem buggy war und beim mergen teilweise Daten verloren gingen
+#Piste 10 wird zu binär umgewandelt weil sie selten genutzt wird und somit ist es deutlich spannender auszuwerten ob es einen Einfluss hat wenn sie genutz wird oder nicht statt wie viele Flüge
 df_abfluege["datum"] = df_abfluege["datum"].astype(str).str.strip()
 df_abfluege["datum"] = pd.to_datetime(df_abfluege["datum"], format="%d.%m.%y").dt.normalize()
+df_abfluege["piste_10_binär"] = np.where(df_abfluege["piste_10"] > 0, 1, 0)
 
 #Mergen von Dataframes
 df = df.merge(df_abfluege, "left", left_on="Date", right_on="datum")
@@ -43,7 +46,7 @@ df_oil["oil_date"] = pd.to_datetime(df_oil["oil_date"])
 df_oil = df_oil[df_oil["oil_date"] < "2026-03-01"]
 df = df.merge(df_oil, "left", left_on="Date", right_on="oil_date")
 
-df = df[["Date", "Avg Departure Schedule Delay", "anzahl_abfluege", "regen", "windgeschwindigkeit", "maximale_windgeschwindigkeit", "temperatur", "oil_price"]]
+df = df[["Date", "Avg Departure Schedule Delay", "anzahl_abfluege_total", "piste_10", "piste_10_binär", "piste_16", "piste_28", "piste_32", "piste_34", "regen", "windgeschwindigkeit", "maximale_windgeschwindigkeit", "temperatur", "oil_price"]]
 df.to_csv("merge.csv")
 
 #Schauen ob Datum in Ferien, Wochentag und Monat hinzufügen
